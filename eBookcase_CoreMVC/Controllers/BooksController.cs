771df586dd4c3e5b9337abea92e894b40419c2cb
Andrzej Pepłowski <pirx555@gmail.com>
2018-11-14 16:58:33 +0100

    Add Entity Framework Core (service/context). Add BookCoreMVC to database. Change Bootstrap 3.3.7 -> 4.0. Add BooksController. Change default route to BooksController/Index.

---
+++ eBookcase_CoreMVC/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using eBookcase_CoreMVC.Models;
+
+namespace eBookcase_CoreMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eBookcase_CoreMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using eBookcase_CoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eBookcase_CoreMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<BookCoreMVC> BooksCoreMVC { get; set; }
+    }
+}
